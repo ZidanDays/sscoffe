38617029f2ee7a54ec0c9fa59886630dff856bf5
ZidanDays <zidan.dailer@gmail.com>
2024-03-23 14:37:56 +0800

    db

--- conf/koneksi1.php
+++ conf/koneksi1.php
@@ -1,9 +1,9 @@
 <?php
 // Informasi koneksi database
-$servername = "server"; // Lokasi server database
-$username = "sscoffem_admin"; // Nama pengguna database
-$password = "qAj~TUfw8{YO"; // Kata sandi database
-$database = "sscoffem_admin"; // Nama database
+$servername = "localhost"; // Lokasi server database
+$username = "root"; // Nama pengguna database
+$password = ""; // Kata sandi database
+$database = "keisya_ecommerse"; // Nama database
 
 // Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $database);
